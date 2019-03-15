843e7d474e7a5818abbf283a30cca6b5eefbbf57
Max <maksims@skrid.me>
2019-03-15 11:14:01 +0200

    Replace default database connection with SQLite

--- TodoApp/Data/ApplicationDbContext.cs
+++ TodoApp/Data/ApplicationDbContext.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
 
 namespace TodoApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Core Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Core Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
